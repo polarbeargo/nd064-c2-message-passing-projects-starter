319d8d944c4d291af0554d78e988a188ef7f9bb0
Hsin-Wen Chang <bow1226@gmail.com>
2021-10-10 09:16:15 +0800

    fix: Error person not exist

--- modules/connection service/scripts/run_db_command.sh
+++ modules/connection service/scripts/run_db_command.sh
@@ -6,5 +6,5 @@ export CT_DB_NAME=connections
 
 
 cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
-
+cat ./db/udaconnect_public_person.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 cat ./db/udaconnect_public_location.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
